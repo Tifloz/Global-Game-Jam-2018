c632b6c33a16b0997c0abebfcc36da0bbd73a4f2
Tiflo <tiflozion@hotmail.fr>
2018-01-26 23:53:27 +0100

    Ajout du script de déplacement

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    #region "Variables"
+    public GameObject player;
+    public KeyCode[] Keycode_array = new KeyCode[6];
+    public float speed;
+    #endregion
+
+    #region "Keyboard Inputs"
+
+    void InputKeyboard()
+    {
+        if (Input.GetKey(Keycode_array[0]))
+        {
+            player.transform.Translate(Vector2.up * speed);
+        }
+
+        if (Input.GetKey(Keycode_array[1]))
+        {
+            player.transform.Translate(Vector2.right * speed);
+        }
+
+        if (Input.GetKey(Keycode_array[2]))
+        {
+            player.transform.Translate(Vector2.down * speed);
+        }
+
+        if (Input.GetKey(Keycode_array[3]))
+        {
+            player.transform.Translate(Vector2.left * speed);
+        }
+    }
+
+    #endregion
+    // Update is called once per frame
+    void Update () {
+        InputKeyboard();
+	}
+}

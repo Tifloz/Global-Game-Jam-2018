bf828c8c7ddcf5863181eef58b3f9f7e720a5c73
Anthony Leclerc <anthony.leclerc@epitech.eu>
2018-01-27 12:19:48 +0100

    Fix movement inertia with keyboard input

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,69 +6,25 @@ public class Move : MonoBehaviour
 {
     #region "Variables"
     public GameObject player;
-    public KeyCode[] Keycode_array = new KeyCode[3];
     public float speed;
     #endregion
 
     private void Start()
     {
-        Init_Keys();
     }
 
     #region "Keyboard Inputs"
-    void Init_Keys()
-    {
-        Keycode_array[0] = KeyCode.UpArrow;
-        Keycode_array[1] = KeyCode.RightArrow;
-        Keycode_array[2] = KeyCode.DownArrow;
-        Keycode_array[3] = KeyCode.LeftArrow;
-    }
-
-    void InputKeyboard()
-    {
-        if (Input.GetKey(Keycode_array[0]))
-        {
-            player.transform.Translate(Vector2.up * speed);
-        }
-
-        if (Input.GetKey(Keycode_array[1]))
-        {
-            player.transform.Translate(Vector2.right * speed);
-        }
-
-        if (Input.GetKey(Keycode_array[2]))
-        {
-            player.transform.Translate(Vector2.down * speed);
-        }
-
-        if (Input.GetKey(Keycode_array[3]))
-        {
-            player.transform.Translate(Vector2.left * speed);
-        }
-    }
-
+    
     #endregion
 
     #region "XBox Inputs"
 
     void InputXBox()
     {
-        if (Input.GetAxis("Horizontal") > 0.3f)
-        {
-            player.transform.Translate(Vector2.right * speed);
-        }
-        if (Input.GetAxis("Horizontal") < -0.3f)
-        {
-            player.transform.Translate(Vector2.left * speed);
-        }
-        if (Input.GetAxis("Vertical") > 0.3f)
-        {
-            player.transform.Translate(Vector2.up * speed);
-        }
-        if (Input.GetAxis("Vertical") < -0.3f)
-        {
-            player.transform.Translate(Vector2.down * speed);
-        }
+        var hAxis = Input.GetAxis("Horizontal");
+        var vAxis = Input.GetAxis("Vertical");
+        player.transform.Translate(Vector2.right * speed * hAxis);
+        player.transform.Translate(Vector2.up * speed * vAxis);
     }
 
     #endregion
@@ -76,7 +32,7 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        InputKeyboard();
+  //      InputKeyboard();
         InputXBox();
     }
 }

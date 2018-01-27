9463b02c93cd3ce16c07c6d6ab1ad205c67ef699
Tiflo <tiflozion@hotmail.fr>
2018-01-27 02:49:48 +0100

    Je vais frapper ces Rigidbody2D mais le script pour les villes marche

--- Assets/Scripts/City.cs
+++ Assets/Scripts/City.cs
@@ -8,15 +8,18 @@ public class City : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         light = GetComponentInChildren<Light>();
+        light.intensity = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    private void OnTriggerEnter(Collider other)
+
+    // Update is called once per frame
+    void Update()
     {
-        if (other.tag == "Player" && light.intensity == 0)
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Lul");
+        if (collision.tag == "Player" && light.intensity == 0)
             light.intensity = 3;
     }
 }

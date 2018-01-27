9f80a304fa1e3bdf574fb311ce44ba2b3c2af4c8
Anthony Leclerc <anthony.leclerc@epitech.eu>
2018-01-27 12:59:22 +0100

    Adding Gem Prefab and mana interaction

---
+++ Assets/Components/Mana.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mana : MonoBehaviour {
+
+    public int ManaMax = 0;
+    public int ManaCurrent = 0;
+	// Use this for initialization
+
+    public void PickUpGem()
+    {
+        Debug.Log("PickUp Gem");
+        ManaMax += 1;
+        ManaCurrent = ManaMax;
+    }
+
+}
---
+++ Assets/Scripts/GemBonus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemBonus : MonoBehaviour {
+
+    public GameObject Gem;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Mana>().PickUpGem();
+            Destroy(Gem);
+        }
+    }
+}

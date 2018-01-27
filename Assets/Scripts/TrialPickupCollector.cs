81083d41ef3b1fdbf74f9967791c30f4d5abaf26
Anthony Leclerc <anthony.leclerc@epitech.eu>
2018-01-27 17:15:52 +0100

    Trial: Spawner of light entities -> Trigger the trial with the Action button (Keyboard E or Controller Y), Collect all entities and return to the trial altar with every light

---
+++ Assets/Scripts/TrialPickupCollector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrialPickupCollector : MonoBehaviour {
+
+    // Use this for initialization
+    public int PickupGoal;
+    public int Current;
+
+    public void Start()
+    {
+        Current = 0;
+    }
+
+    public void Pickup()
+    {
+        if (Current < PickupGoal)
+            Current += 1;
+    }
+
+    public void Reset()
+    {
+        Current = 0;
+    }
+
+    public bool Completed()
+    {
+        return Current == PickupGoal;
+    }
+}

60eb646ad382527399c96cac74aea8123e57d851
Tiflo <tiflozion@hotmail.fr>
2018-01-28 14:02:15 +0100

    Ajout de l'audio

---
+++ Assets/Scripts/AudioRend.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioRend : MonoBehaviour {
+    public AudioClip source;
+    private AudioSource bruh;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            bruh = collision.GetComponent<AudioSource>();
+            bruh.clip = source;
+            if (!bruh.isPlaying)
+            {
+                bruh.Play();
+                bruh.volume = 0.5f;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        StartCoroutine(Counter());
+    }
+    IEnumerator Counter()
+    {
+        while (bruh.volume >= 0)
+        {
+            bruh.volume = -0.15f;
+            yield return new WaitForSeconds(1);
+        }
+        if (bruh.volume <= 0)
+            bruh.Stop();
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
